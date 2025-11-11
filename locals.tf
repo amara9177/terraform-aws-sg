2728e9d7e976328518c3319817c512175b19921c
amara <rama@gmail.com>
2025-11-11 23:22:49 +0530

    message

---
+++ locals.tf
@@ -0,0 +1,8 @@
+locals {
+    common_tags= {
+        project = var.project_name
+        Environment = var.environment
+        Terraform = true
+    }
+    common_name_suffix = "${var.project_name}-${var.environment}"
+}
---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "sg_name"{
+    type = string
+}
+variable "sg_description"{
+    type = string
+}
+variable "vpc_id" {
+    type = string
+}
+variable "sg_tags"{
+    type = map 
+   default  = {}
+}
+variable "project_name"{
+    type = string
+}
+variable "environment"{
+    type = string
+}   
